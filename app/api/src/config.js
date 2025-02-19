1e27ceaa1789d4f363b6beed6f45d13df905917d
Shramee Srivastav <shramee.srivastav@gmail.com>
2025-02-19 11:47:54 +0530

    chore: backend for 4000

--- app/api/src/config.js
+++ app/api/src/config.js
@@ -7,7 +7,7 @@ export const PG_PASSWORD = process.env.PG_PASSWORD || "postgres";
 export const PG_DATABASE = process.env.PG_DATABASE || "starklings-db";
 export const PG_DATABASE_SSL = process.env.PG_DATABASE_SSL === `true`;
 
-export const ORIGIN = process.env.ORIGIN || "http://localhost:5173";
+export const ORIGIN = process.env.ORIGIN || "http://localhost:4000";
 
 export const URL_GITHUB_STARKLINGS = process.env.URL_GITHUB_STARKLINGS;
 
